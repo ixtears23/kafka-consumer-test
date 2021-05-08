f41b27ef3ecb415986db2f469c0410096d89f304
연구개발팀장 <ojs@ibdata.co.kr>
2021-05-08 22:27:41 +0900

    project init

---
+++ src/main/java/junseok/rest/demoinflearnrestapi/JunseokApplication.java
@@ -0,0 +1,13 @@
+package junseok.rest.demoinflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunseokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunseokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junseok/rest/demoinflearnrestapi/JunseokApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.rest.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunseokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
