92c69e756a05b6eab779b925ebc83dd240195d3b
JunseokOh <ixtears23@icloud.com>
2024-01-14 16:38:10 +0900

    kafka-producer-sync-callback

--- test-kafka/src/main/java/junseok/snr/kafka/SimpleProducer.java
+++ test-kafka/src/main/java/junseok/snr/kafka/SimpleProducer.java
@@ -3,27 +3,32 @@ package junseok.snr.kafka;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class SimpleProducer {
-    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
-    private final static String TOPIC_NAME ="test";
-    private final static String BOOTSTRAP_SERVER = "test.kafka:9092";
+    private static final Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private static final String TOPIC_NAME ="test";
+    private static final String BOOTSTRAP_SERVER = "test.kafka:9092";
 
-    public static void main(String[] args) {
-        Properties configs = new Properties();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Properties configs = new Properties();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
 
-        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
 
-        ProducerRecord<String, String> record = new ProducerRecord<>("test", "Pangyo", "23");
-        producer.send(record);
+        final String messageValue = "testMessage";
+        final ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue);
+        RecordMetadata recordMetadata = producer.send(record).get();
+        logger.info(recordMetadata.toString());
         producer.flush();
         producer.close();
     }
