3399d0434be8b5325e48bf92b6cc7d2b901702e3
JunseokOh <ixtears23@icloud.com>
2024-01-15 04:29:37 +0900

    kafka-consumer-sync-commit

--- kafka-consumer/src/main/java/org/example/SimpleConsumer.java
+++ kafka-consumer/src/main/java/org/example/SimpleConsumer.java
@@ -26,6 +26,7 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
@@ -35,6 +36,7 @@ public class SimpleConsumer {
             for (ConsumerRecord<String, String> record : records) {
                 logger.info("{}", record);
             }
+            consumer.commitSync();
         }
 
     }
