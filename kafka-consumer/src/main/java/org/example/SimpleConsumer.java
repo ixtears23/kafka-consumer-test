52cc6a9d4d1124f550e2e6264f1d13e13114ce01
JunseokOh <ixtears23@icloud.com>
2024-01-15 03:52:43 +0900

    simple-kafka-consumer

---
+++ kafka-consumer/src/main/java/org/example/SimpleConsumer.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+    private static final String TOPIC_NAME = "test";
+    private static final String BOOTSTRAP_SERVERS = "test.kafka:9092";
+    private static final String GROUP_ID = "test-group";
+
+    public static void main(String[] args) {
+        final Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(5));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("{}", record);
+            }
+        }
+
+    }
+}
